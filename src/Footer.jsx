85fdcff7aed243d017ae71ecf6d1b74021c48363
hemanth-naidu-gandham <krishnagandham87@gmail.com>
2025-06-25 12:20:39 +0530

    initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,36 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="icons">
+        <h2 className="footer-heading">
+          <span>Follow us on:</span>
+        </h2>
+        <div className="icon-grid">
+          <a href="https://github.com" className="icon-link">
+            <i className="fa-brands fa-github"></i> GitHub:
+          </a>
+          <a href="https://www.linkedin.com" className="icon-link">
+            <i className="fa-brands fa-linkedin"></i> LinkedIn:
+          </a>
+          <a href="https://www.facebook.com" className="icon-link">
+            <i className="fa-brands fa-facebook"></i> Facebook:
+          </a>
+          <a href="https://www.instagram.com" className="icon-link">
+            <i className="fa-brands fa-instagram"></i> Instagram:
+          </a>
+          <a href="https://www.youtube.com" className="icon-link">
+            <i className="fa-brands fa-youtube"></i> YouTube:
+          </a>
+          <a href="https://twitter.com" className="icon-link">
+            <i className="fa-brands fa-twitter"></i> Twitter:
+          </a>
+          <a href="https://www.gmail.com" className="icon-link">
+          <i className="fa-solid fa-envelope"></i> Email:
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
