6469b2a4aaaef44170fa70f3b23a3fe0332cbcdc
hemanth-naidu-gandham <krishnagandham87@gmail.com>
2025-06-25 15:44:51 +0530

    initial commit

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,30 +4,33 @@ function Footer() {
     <footer className="footer">
       <div className="icons">
         <h2 className="footer-heading">
-          <span>Follow us on:</span>
+          <span><center>Follow us on:</center></span>
         </h2>
         <div className="icon-grid">
           <a href="https://github.com" className="icon-link">
-            <i className="fa-brands fa-github"></i> GitHub:
+            <i className="fa-brands fa-github"></i> GitHub
           </a>
           <a href="https://www.linkedin.com" className="icon-link">
-            <i className="fa-brands fa-linkedin"></i> LinkedIn:
+            <i className="fa-brands fa-linkedin"></i> LinkedIn
           </a>
           <a href="https://www.facebook.com" className="icon-link">
-            <i className="fa-brands fa-facebook"></i> Facebook:
+            <i className="fa-brands fa-facebook"></i> Facebook
           </a>
           <a href="https://www.instagram.com" className="icon-link">
-            <i className="fa-brands fa-instagram"></i> Instagram:
+            <i className="fa-brands fa-instagram"></i> Instagram
           </a>
           <a href="https://www.youtube.com" className="icon-link">
-            <i className="fa-brands fa-youtube"></i> YouTube:
+            <i className="fa-brands fa-youtube"></i> YouTube
           </a>
           <a href="https://twitter.com" className="icon-link">
-            <i className="fa-brands fa-twitter"></i> Twitter:
+            <i className="fa-brands fa-twitter"></i> Twitter
           </a>
           <a href="https://www.gmail.com" className="icon-link">
-          <i className="fa-solid fa-envelope"></i> Email:
+          <i className="fa-solid fa-envelope"></i> Email
           </a>
+          <a href="https://www.whatsapp.com" className="icon-link">
+          <i class="fa-brands fa-whatsapp"></i>Whatsapp
+           </a>
         </div>
       </div>
     </footer>
